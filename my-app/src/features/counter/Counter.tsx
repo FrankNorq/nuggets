ecdcd2c1126248a82a9a21b7eb7b81b0303bf84e
Frank <Frankuik@hotmail.com>
2025-02-19 11:51:42 +0100

    12312313

--- my-app/src/features/counter/Counter.tsx
+++ my-app/src/features/counter/Counter.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-
-import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import styles from "./Counter.module.css"
+import  "../counter/Counter.module.css"
 import {increment,decrement,setCount} from "../../app/createAppSlice"
 function CounterRender() {
     const dispatch = useDispatch()
@@ -11,6 +8,7 @@ function CounterRender() {
     return ( <div>
         <button onClick={()=>{dispatch(increment())}}>Increment</button>
         <button onClick={()=>{dispatch(decrement())}}>Decrement</button>
+        <button onClick={()=>{dispatch(setCount(0))}}>Reset button</button>
         <p>{count}</p>
         
     </div> );
